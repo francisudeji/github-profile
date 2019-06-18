147ed69cab7cc4d48735bb910be25c71b4d240a0
francisudeji <francis.udejiofficial@gmail.com>
2019-06-18 21:13:56 +0100

    Update path to import in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { AppProvider } from './context'
-import Home from './pages/home'
-import Profile from './pages/profile'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
 
 function App() {
   return (
