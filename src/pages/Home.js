bc272957cc7fef3fd22b85bb03b5a2ba82a0cabc
francisudeji <francis.udejiofficial@gmail.com>
2019-06-18 19:00:22 +0100

    Now we fetch data on component mount in pprofile page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,73 +1,80 @@
 import React, { useEffect, useState } from 'react'
-import netlify from 'netlify-auth-providers'
 import { FaGithub } from 'react-icons/fa'
+import axios from 'axios'
+import { authenticateWithGithub } from '../utils'
+import HomeWrapper from '../components/home-wrapper'
 import '../styles/home.css'
 
-function HomeWrapper({ children }) {
-  return (
-    <div className='home'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-xs-12 col-sm-12 col-md-6 col-lg-6 mx-auto wrapper'>
-            {children}
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-function Home() {
+function Home({ history }) {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
-
-  async function authWithGitHub() {
-    return new Promise((resolve, reject) => {
-      var authenticator = new netlify({
-        site_id: 'bb636699-af55-440f-84e8-0076e8f1aab5'
-      })
-      authenticator.authenticate(
-        { provider: 'github', scope: 'public_repo,read:org,read:user' },
-        function(err, data) {
-          if (err) {
-            console.log(err)
-            reject(err)
-          }
-          console.log(data)
-          resolve(data)
-        }
-      )
-    })
-  }
+  const [token, setToken] = useState(null)
+  const [username, setUsername] = useState('')
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     const token = localStorage.getItem('github-token')
     if (token !== null) {
       setIsAuthenticated(true)
+      setToken(token)
     }
   }, [isAuthenticated])
 
   async function getToken() {
-    const data = await authWithGitHub().catch(error => {
+    const data = await authenticateWithGithub().catch(error => {
       console.log({ error })
     })
     localStorage.setItem('github-token', data.token)
     setIsAuthenticated(true)
   }
 
+  function handleChange(e) {
+    setUsername(e.target.value)
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    const baseURL = 'https://api.github.com/users'
+    const headers = { Authorization: `bearer ${token}` }
+    setError(null)
+    setUsername('')
+
+    axios
+      .get(`${baseURL}/${username}`, { headers })
+      .then(res => {
+        history.push(`/${username}`)
+      })
+      .catch(({ response }) => {
+        setUsername('')
+        setError(response.statusText)
+      })
+  }
+
   if (isAuthenticated) {
     return (
       <HomeWrapper>
-        <form>
+        {error !== null && (
+          <div className='alert alert-danger fade show' role='alert'>
+            {error}
+          </div>
+        )}
+        <form onSubmit={handleSubmit}>
           <div className='form-group'>
+            <label htmlFor='username'>Username</label>
             <input
-              type='text'
+              type='search'
               className='form-control'
-              name='username'
+              id='username'
+              value={username}
+              onChange={handleChange}
               placeholder='Search by typing a username'
               autoFocus
             />
           </div>
-          <input type='submit' className='btn btn-success btn-block' />
+          <input
+            type='submit'
+            value='Search'
+            className='btn btn-success btn-block'
+          />
         </form>
       </HomeWrapper>
     )
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,118 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getUserProfile } from '../utils'
+import { Link } from 'react-router-dom'
+import { FaUsers, FaMapMarker, FaEnvelope, FaLink } from 'react-icons/fa'
+import '../styles/profile.css'
 
-function Profile() {
+function Profile({ match, location }) {
+  let username = match.params.username
+  const [{ basicInfo, allRepos, starredRepos }, setState] = useState({})
+  const [hasAllData, setHasAllData] = useState(false)
+
+  useEffect(() => {
+    getUserProfile(username)
+      .then(data => {
+        setState({ ...data })
+        setHasAllData(true)
+      })
+      .catch(err => console.log(err))
+  }, [username])
+
   return (
     <div className='profile'>
-      <h1>Profile Page</h1>
+      <div className='container mt-5'>
+        {hasAllData ? (
+          <div className='row'>
+            <div className='col-xs-12 col-sm-12 col-md-4 col-lg-4'>
+              <pre>{JSON.stringify(allRepos, null, 2)}</pre>
+              {/* <div className='img-container'>
+                <img
+                  src={basicInfo.avatar_url}
+                  style={{ maxWidth: '270px', width: '100%' }}
+                  width='270'
+                  alt={basicInfo.login}
+                />
+              </div>
+              <h3 className='mt-3'>{basicInfo.name}</h3>
+              <p className='lead'>{basicInfo.login}</p>
+              <button className='btn btn-secondary btn-block mb-3'>
+                Follow
+              </button>
+              <p>{basicInfo.bio}</p>
+              {basicInfo.company !== null && (
+                <p>
+                  <FaUsers /> {basicInfo.company}
+                </p>
+              )}
+              {basicInfo.location !== null && (
+                <p>
+                  <FaMapMarker /> {basicInfo.location}
+                </p>
+              )}
+              {basicInfo.email !== null && (
+                <p>
+                  <FaEnvelope /> {basicInfo.email}
+                </p>
+              )}
+              {basicInfo.blog !== '' && (
+                <p>
+                  <FaLink /> <a href={basicInfo.blog}>{basicInfo.blog}</a>
+                </p>
+              )} */}
+            </div>
+            <div className='col-xs-12 col-sm-12 col-md-8 col-lg-8'>
+              {/* {hasAllData && <pre>{JSON.stringify(basicInfo, null, 2)}</pre>} */}
+
+              <ul className='nav nav-tabs'>
+                <li className='nav-item'>
+                  <Link
+                    className='nav-link text-dark active-tab'
+                    to={`/${username}/repositories`}
+                  >
+                    Repositories{' '}
+                    <span className='badge badge-light'>{allRepos.length}</span>
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link
+                    className='nav-link text-dark'
+                    to={`/${username}/stars`}
+                  >
+                    Stars{' '}
+                    <span className='badge badge-light'>
+                      {starredRepos.length}
+                    </span>
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link
+                    className='nav-link text-dark'
+                    to={`/${username}/followers`}
+                  >
+                    Followers{' '}
+                    <span className='badge badge-light'>
+                      {basicInfo.followers}
+                    </span>
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link
+                    className='nav-link text-dark'
+                    to={`/${username}/following`}
+                  >
+                    Following{' '}
+                    <span className='badge badge-light'>
+                      {basicInfo.following}
+                    </span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        ) : (
+          <p className='lead'>Loading...</p>
+        )}
+      </div>
     </div>
   )
 }
