3ae1ea977f413222b9c56ec6e8401c67e7dd198a
francisudeji <francis.udejiofficial@gmail.com>
2019-06-18 13:16:28 +0100

    Setup react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Login from './pages/Login.js'
+import Profile from './pages/Profile'
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Github Profile</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/profile' component={Profile} />
+      </Switch>
+    </Router>
   )
 }
 
