bd77e58f6e6e1ef132da209d08ac4b7c79a06cfa
francisudeji <francis.udejiofficial@gmail.com>
2019-06-19 22:01:28 +0100

    Display followers and following

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,7 +5,8 @@ import {
   FaMapMarker,
   FaEnvelope,
   FaLink,
-  FaStar
+  FaStar,
+  FaMarker
 } from 'react-icons/fa'
 import distanceInWords from 'date-fns/distance_in_words'
 import isThisYear from 'date-fns/is_this_year'
@@ -28,7 +29,10 @@ function TabContents({ username, pathname, children }) {
 
 function Profile({ match, location }) {
   let username = match.params.username
-  const [{ basicInfo, allRepos, starredRepos }, setState] = useState({})
+  const [
+    { basicInfo, allRepos, starredRepos, followers, following },
+    setState
+  ] = useState({})
   const [hasAllData, setHasAllData] = useState(false)
   const [err, setErr] = useState(null)
 
@@ -53,19 +57,11 @@ function Profile({ match, location }) {
 
   return (
     <div className='profile'>
-      <div
-        className='container mt-5'
-        style={{
-          display: 'flex',
-          minHeight: '100vh',
-          alignItems: 'center',
-          justifyContent: 'center'
-        }}
-      >
+      <div className='container mt-5'>
         {hasAllData ? (
           <div className='row'>
             <div className='col-xs-12 col-sm-12 col-md-4 col-lg-4'>
-              {console.log(starredRepos)}
+              {console.log(followers, following)}
               <div className='img-container'>
                 <img
                   src={basicInfo.avatar_url}
@@ -260,13 +256,159 @@ function Profile({ match, location }) {
                     </div>
                   ))}
                 </div>
-                <div className='followers'>followers</div>
-                <div className='following'>following</div>
+                <div className='followers'>
+                  {followers.map(follower => (
+                    <div
+                      className='card'
+                      key={follower.id}
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        borderLeft: 0,
+                        borderTop: 0,
+                        borderRight: 0
+                      }}
+                    >
+                      <div
+                        className='card-header bg-white'
+                        style={{ border: 0 }}
+                      >
+                        <img
+                          src={follower.avatar_url}
+                          height='50'
+                          width='50'
+                          alt={follower.login}
+                        />
+                      </div>
+                      <div className='card-body'>
+                        <h3 style={{ fontSize: '1.2rem' }}>
+                          <a href={follower.html_url}>
+                            <strong>{follower.name}</strong>{' '}
+                            <span>{follower.login}</span>
+                          </a>
+                        </h3>
+                        <p className='d-inline-block text-gray mb-2 pr-4'>
+                          {follower.bio}
+                        </p>
+                        <p>
+                          {follower.company && (
+                            <small className='small-text'>
+                              <FaUsers style={{ marginBottom: '2px' }} />{' '}
+                              {follower.company}
+                            </small>
+                          )}
+                          {follower.location && (
+                            <small className='small-text'>
+                              <FaMarker style={{ marginBottom: '2px' }} />{' '}
+                              {follower.location}
+                            </small>
+                          )}
+                        </p>
+                      </div>
+                      <div
+                        className='card-footer bg-white'
+                        style={{ border: 0 }}
+                      >
+                        <button
+                          style={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            color: '#24292e',
+                            backgroundColor: '#eff3f6'
+                          }}
+                          className='btn btn-light btn-sm'
+                        >
+                          Follow
+                        </button>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+                <div className='following'>
+                  {following.map((_following, i) => (
+                    <div
+                      className='card'
+                      key={i}
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        borderLeft: 0,
+                        borderTop: 0,
+                        borderRight: 0
+                      }}
+                    >
+                      {console.log({ _following })}
+                      <div
+                        className='card-header bg-white'
+                        style={{ border: 0 }}
+                      >
+                        <img
+                          src={_following.avatar_url}
+                          height='50'
+                          width='50'
+                          alt={_following.login}
+                        />
+                      </div>
+                      <div className='card-body'>
+                        <h3 style={{ fontSize: '1.2rem' }}>
+                          <a href={_following.html_url}>
+                            <strong>{_following.name}</strong>{' '}
+                            <span>{_following.login}</span>
+                          </a>
+                        </h3>
+                        <p className='d-inline-block text-gray mb-2 pr-4'>
+                          {_following.bio}
+                        </p>
+                        <p>
+                          {_following.company && (
+                            <small className='small-text'>
+                              <FaUsers style={{ marginBottom: '2px' }} />{' '}
+                              {_following.company}
+                            </small>
+                          )}
+                          {_following.location && (
+                            <small className='small-text'>
+                              <FaMarker style={{ marginBottom: '2px' }} />{' '}
+                              {_following.location}
+                            </small>
+                          )}
+                        </p>
+                      </div>
+                      <div
+                        className='card-footer bg-white'
+                        style={{ border: 0 }}
+                      >
+                        <button
+                          style={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            color: '#24292e',
+                            backgroundColor: '#eff3f6'
+                          }}
+                          className='btn btn-light btn-sm'
+                        >
+                          Follow
+                        </button>
+                      </div>
+                    </div>
+                  ))}
+                </div>
               </TabContents>
             </div>
           </div>
         ) : (
-          <div className='lead text-center'>
+          <div
+            className='lead text-center'
+            style={{
+              marginTop: '50vh'
+            }}
+          >
             {err === null ? (
               <>
                 <p className='lead'>Fetching details for {username}</p>
